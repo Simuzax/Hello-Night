6b346a98906db84681ea6f8e22fd4e9a58d45a17
Simuzax <miguelselbackcoronel@gmail.com>
2019-12-18 16:52:36 -0300

    1st Commit

---
+++ Treason/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((speed * (transform.position - target.position)));
+    }
+}
---
+++ Treason/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float walkSpeed;
+    public float runSpeed;
+
+    Vector2 input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(runSpeed * input * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(walkSpeed * input * Time.deltaTime);
+        }
+    }
+}
